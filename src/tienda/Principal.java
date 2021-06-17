095f5ee2e2dd9800c5756dd8ea508bf54e4dbe31
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-17 14:24:29 -0600

    Se ha modificado Principal.java

--- src/tienda/Principal.java
+++ src/tienda/Principal.java
@@ -1,6 +1,7 @@
 package src.tienda;
 
 import src.clientes.VectorClientes;
+import src.peliculas.VectorPeliculas;
 
 public class Principal {
 
@@ -9,6 +10,7 @@ public class Principal {
     }
 
     private VectorClientes tablaClientes = new VectorClientes();
+    private VectorPeliculas tablaPeliculas = new VectorPeliculas();
 
     public Principal(){
 
@@ -20,9 +22,18 @@ public class Principal {
             System.out.println("2) Mostrar clientes");
             System.out.println("3) Ordenar Clientes Ascente");
             System.out.println("4) Ordenar Clientes Descendente");
+            
+            System.out.println("5) Ingreso de peliculas");
+            System.out.println("6) Mostrar peliculas");
+            System.out.println("7) Ordenar Peliculas Ascendente");
+            System.out.println("8) Ordenar Peliculas Descendente");
+            
+            System.out.println("9) Prestamo de peliculas");
+            System.out.println("10) Mostrar Prestamos de peliculas");
+            
             System.out.println("-1) Salir");
             System.out.println("\n");
-            menu = IngresoDatos.getEntero("Ingrese la opción ", true);
+            menu = IngresoDatos.getEntero("Ingrese la opcion ", true);
 
             if (menu == 1 ){
                 //ingreso de datos
@@ -44,10 +55,38 @@ public class Principal {
                 //mostrar clientes
                 System.out.println("Desordenado:");
                 tablaClientes.mostrarClientes();
-                System.out.println("\n\nOrdenado:");
+                System.out.println("\nOrdenado:");
                 tablaClientes.ordenarPorNombre(false);
                 tablaClientes.mostrarClientes();
             }
+            if (menu == 5) {            //1
+                //ingreso de datos
+                tablaPeliculas.agregarPelicula();
+            }
+            if (menu == 6) {
+                //mostrar pelis
+                tablaPeliculas.mostrarPeliculas();
+            }
+            if (menu == 7) {
+                //mostrar pelis ascendente
+                System.out.println("Desordenado:");
+                tablaPeliculas.mostrarPeliculas();
+                System.out.println("\nOrdenado:");
+                tablaPeliculas.ordenarPorNombrePeli(true);
+                tablaClientes.mostrarClientes();
+            }
+            if (menu == 8) {
+                //mostrar pelis descendente
+                System.out.println("Desordenado:");
+                tablaPeliculas.mostrarPeliculas();
+                System.out.println("\nOrdenado:");
+                tablaPeliculas.ordenarPorNombrePeli(false);
+                tablaPeliculas.mostrarPeliculas();
+            }
+            if (menu == 9) {
+                tablaPeliculas.agregarPeliculaPrestada();
+            }
+            
 
         }
 
